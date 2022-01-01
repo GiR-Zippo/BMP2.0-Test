18a8f5049c01786b73199309e83393e5414e5571
GiR_Zippo <gir_zippo@hellokitty.com>
2022-01-01 22:14:45 +0100

    [Classic Ui] ChatBox uses DeferredScrolling
    https://github.com/GiR-Zippo/BMP2.0-Test/issues/3

--- BardMusicPlayer.Ui/UI_Classic/Classic_Statistics.cs
+++ BardMusicPlayer.Ui/UI_Classic/Classic_Statistics.cs
@@ -1,9 +1,5 @@
 ﻿using BardMusicPlayer.Ui.Functions;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace BardMusicPlayer.Ui.Classic
