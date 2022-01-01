ae1b909d9e4ef4b0d8aa774a02c087b3d4500738
GiR_Zippo <gir_zippo@hellokitty.com>
2022-01-01 20:12:36 +0100

    [App] set a proper name and icon
    Fixing: https://github.com/GiR-Zippo/BMP2.0-Test/issues/2

--- BardMusicPlayer.Ui/Properties/AssemblyInfo.cs
+++ BardMusicPlayer.Ui/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Ui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BMPAmp")]
+[assembly: AssemblyDescription("Testversion of the BMP2.0 libs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ui")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("GiR-Zippo")]
+[assembly: AssemblyProduct("BMPAmp")]
+[assembly: AssemblyCopyright("Copyleft ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
